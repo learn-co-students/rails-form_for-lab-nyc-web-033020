fcb2c5d5b4c6fe0f200c6b1d1db461d48776d660
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-04-26 21:57:52 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+Student.delete_all
+SchoolClass.delete_all
+
+20.times { Student.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name) }
+7.times { SchoolClass.create(title: Faker::Educator.subject, room_number:Faker::Number.number(digits: 3))}
